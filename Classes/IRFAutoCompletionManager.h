efa06c25bb444c6bdda26fad24b99009044883ef
Fabio Pelosin <fabiopelosin@gmail.com>
2013-11-11 21:10:29 +0100

    Add warnings with liftoff

--- Classes/IRFAutoCompletionManager.h
+++ Classes/IRFAutoCompletionManager.h
@@ -55,5 +55,3 @@
 - (void)autoCompletionTextFieldManagerDidPerformSubstitution:(IRFAutoCompletionManager*)manager;
 
 @end
-
-//------------------------------------------------------------------------------

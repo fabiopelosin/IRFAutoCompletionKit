d4eee2bd1f40c518dfef1043355923a3ac940119
Fabio Pelosin <fabiopelosin@gmail.com>
2013-11-17 19:02:08 +0100

    [Tests] Organize

--- Project/IRFAutoCompletionKitTests/IRFAutoCompletionProviderTests.m
+++ Project/IRFAutoCompletionKitTests/IRFAutoCompletionProviderTests.m
@@ -29,66 +29,69 @@ describe(@"IRFAutoCompletionProvider", ^{
     //--------------------------------------------------------------------------
 
     context(@"-shouldStartAutoCompletionForString", ^{
+        context(@"should start", ^{
+            it(@"starts after the start character", ^{
+                NSString *string = @":";
+                BOOL result = [sut shouldStartAutoCompletionForString:string];
+                [[theValue(result) should] beTrue];
+            });
 
-        it(@"starts after the start character", ^{
-            NSString *string = @":";
-            BOOL result = [sut shouldStartAutoCompletionForString:string];
-            [[theValue(result) should] beTrue];
+            it(@"starts after the user typed the start character in a string", ^{
+                NSString *string = @"Some string :";
+                BOOL result = [sut shouldStartAutoCompletionForString:string];
+                [[theValue(result) should] beTrue];
+            });
+
+            it(@"starts after the start character if a completion is still in progress", ^{
+                NSString *string = @"Some string :tumb";
+                BOOL result = [sut shouldStartAutoCompletionForString:string];
+                [[theValue(result) should] beTrue];
+            });
+
+            it(@"is is not confused by previous completions", ^{
+                NSString *string = @"sometext :+1: :thumbs";
+                BOOL result = [sut shouldStartAutoCompletionForString:string];
+                [[theValue(result) should] beTrue];
+            });
         });
 
-        it(@"doesnt' starts if the start character could not be found", ^{
-            NSString *string = @"Some string";
-            BOOL result = [sut shouldStartAutoCompletionForString:string];
-            [[theValue(result) should] beFalse];
-        });
+        context(@"should not start", ^{
+            it(@"doesnt' starts if the start character could not be found", ^{
+                NSString *string = @"Some string";
+                BOOL result = [sut shouldStartAutoCompletionForString:string];
+                [[theValue(result) should] beFalse];
+            });
 
-        it(@"starts after the user typed the start character in a string", ^{
-            NSString *string = @"Some string :";
-            BOOL result = [sut shouldStartAutoCompletionForString:string];
-            [[theValue(result) should] beTrue];
-        });
+            it(@"doesn't starts with a space after a completed emoji", ^{
+                NSString *string = @"sometext :thumbs: ";
+                BOOL result = [sut shouldStartAutoCompletionForString:string];
+                [[theValue(result) should] beFalse];
+            });
 
-        it(@"starts after the start character if a completion is still in progress", ^{
-            NSString *string = @"Some string :tumb";
-            BOOL result = [sut shouldStartAutoCompletionForString:string];
-            [[theValue(result) should] beTrue];
-        });
+            it(@"doesn't start after a separation character", ^{
+                NSString *string = @"sometext :thmbs: up";
+                BOOL result = [sut shouldStartAutoCompletionForString:string];
+                [[theValue(result) should] beFalse];
+            });
 
-        it(@"is is not confused by previous completions", ^{
-            NSString *string = @"sometext :+1: :thumbs";
-            BOOL result = [sut shouldStartAutoCompletionForString:string];
-            [[theValue(result) should] beTrue];
-        });
+            it(@"doesn't start when the completion has been terminated and has the same charater for the start and the end", ^{
+                NSString *string = @"sometext :thumbs:";
+                BOOL result = [sut shouldStartAutoCompletionForString:string];
+                [[theValue(result) should] beFalse];
+            });
 
-        it(@"doesn't starts with a space after a completed emoji", ^{
-            NSString *string = @"sometext :thumbs: ";
-            BOOL result = [sut shouldStartAutoCompletionForString:string];
-            [[theValue(result) should] beFalse];
-        });
-
-        it(@"doesn't start after a separation character", ^{
-            NSString *string = @"sometext :thmbs: up";
-            BOOL result = [sut shouldStartAutoCompletionForString:string];
-            [[theValue(result) should] beFalse];
-        });
-
-        it(@"doesn't start when the completion has been terminated and has the same charater for the start and the end", ^{
-            NSString *string = @"sometext :thumbs:";
-            BOOL result = [sut shouldStartAutoCompletionForString:string];
-            [[theValue(result) should] beFalse];
-        });
-
-        it(@"doesn't start if the start character is not preceeded by a separator character", ^{
-            NSString *string = @"sometext:thu";
-            BOOL result = [sut shouldStartAutoCompletionForString:string];
-            [[theValue(result) should] beFalse];
+            it(@"doesn't start if the start character is not preceeded by a separator character", ^{
+                NSString *string = @"sometext:thu";
+                BOOL result = [sut shouldStartAutoCompletionForString:string];
+                [[theValue(result) should] beFalse];
+            });
         });
     });
 
     //--------------------------------------------------------------------------
 
     context(@"-shouldEndAutoCompletionForString", ^{
-        context(@"Finalized completion", ^{
+        context(@"should end", ^{
             it(@"ends with a separator character", ^{
                 BOOL result = [sut shouldEndAutoCompletionForString:@":+1: "];
                 [[theValue(result) should] beTrue];
@@ -112,7 +115,7 @@ describe(@"IRFAutoCompletionProvider", ^{
             });
         });
 
-        context(@"Not finalized completion", ^{
+        context(@"should not end", ^{
             it(@"doesn't end with a completin at the start of the string", ^{
                 BOOL result = [sut shouldEndAutoCompletionForString:@":thumbs"];
                 [[theValue(result) should] beFalse];
